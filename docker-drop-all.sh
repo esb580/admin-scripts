ef85fc39ad7de6ce9212f803a13fbaeb477e2466
esb580 <47366670+esb580@users.noreply.github.com>
2019-02-07 19:02:41 -0800

    Update docker-drop-all.sh
    Added Warning

--- docker-drop-all.sh
+++ docker-drop-all.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#WARNING: Don't run this unless you want to remove all your containers. 
+#  That would be bad.
+#Description: Remove all docker containers.  Too lazy to type.
 docker ps -a | awk '{ print $1 }' | grep -v CONTAINER | xargs -I{} docker rm {}
