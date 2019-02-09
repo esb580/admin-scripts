0da8d04286b25248a523c9756c0f4c34c77a089e
esb580 <47366670+esb580@users.noreply.github.com>
2019-02-08 18:26:19 -0800

    Delete docker-drop-all.sh

--- docker-drop-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#WARNING: Don't run this unless you want to remove all your containers. 
-#  That would be bad.
-#Description: Remove all docker containers.  Too lazy to type.
-docker ps -a | awk '{ print $1 }' | grep -v CONTAINER | xargs -I{} docker rm {}
