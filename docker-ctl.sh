7500668b928b101269f42cef053cd62a890f9205
Han Solo <han@falcon1.hsd1.wa.comcast.net>
2019-02-08 17:29:55 -0800

    Made some corrections and started new function.

--- docker-ctl.sh
+++ docker-ctl.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 #[WARNING] Do not run this...Unless.
-#Desctiption: Stop or Remove all Docker Containers from this system. 
+#Description: Stop or Remove all Docker Containers from this system. 
+
 function docker-stop(){
    echo "STOP ALL containers"
    docker ps -a | awk '{ print $1 }' | grep -v CONTAINER | xargs -I{} docker stop {}
 }
+
 function docker-rm(){
    echo "REMOVE ALL containers"
    docker ps -a | awk '{ print $1 }' | grep -v CONTAINER | xargs -I{} docker rm {}
 }
+
+function docker-rmi(){
+   echo "REMOVE ALL images"
+
+}
+
 while [ true ]; do
+   echo "================================================"
    echo "Docker Actions:"
    echo "1. Remove all containers..Deleting them forever"
    echo "2. Stop all containers"
    echo "3. Exit this loop"
+   echo "================================================"
    read answer
    case $answer in
       1) echo "Execute Delete All Containers"; docker-rm  ;;
